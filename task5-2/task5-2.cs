de57768aae5b315ce802173e9bb702842130a2cf
Vladimir <valenkin@yandex.ru>
2023-02-02 22:07:01 +0500

    task5-2

---
+++ task5-2/task5-2.cs
@@ -0,0 +1,18 @@
+﻿
+//фибоначи
+Console.WriteLine("Задача 2: показать числа Фибоначчи");
+Console.WriteLine();
+Console.WriteLine("Сколько элементов последовательности Фибоначи вы хотите увидеть? ");
+int count = int.Parse(Console.ReadLine()!);
+int[] array = new int[count];
+array[0] = 0;
+array[1] = 1;
+
+for (int index = 2; index < count; index++)
+
+    array[index] = array[index - 1] + array[index - 2];
+
+for (int index = 0; index < count; index++)
+{
+    Console.Write(array[index] + " ");
+}
